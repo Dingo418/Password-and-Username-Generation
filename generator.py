34f83c8f6f7145a85d762c4c11486f527454e8b9
Dingo418 <56755081+Dingo418@users.noreply.github.com>
2022-05-01 17:47:27 +0800

    bit cleaner and clearer code

--- generator.py
+++ generator.py
@@ -1,4 +1,3 @@
-from msilib.schema import Error
 import sys
 import random
 class gen():
@@ -21,7 +20,7 @@ class gen():
                       "    generator <password_length> <amount_of_logins>\n")
                 sys.exit()
         except IndexError:  # if the user did not pass anything threw
-            print("No length specified, using default of 8.")
+            print("No password length specified, using default of 8.")
         gen.genuser_password(amount, pas_length)
     def genlist():
         lower = "abcdefghijklmnopqrstuvwxyz"
@@ -41,19 +40,18 @@ class gen():
         return word_list #returns a list full of words
 
     def genpassword(pasLength):
-        list = gen.genlist() #gets the big list
-        temp = random.choices(list, k=pasLength) #gets a defined amount of random charaters and puts it into a list
+        charaters = gen.genlist() #gets the big string
+        temp = random.choices(charaters, k=pasLength) #gets a defined amount of random charaters and puts it into a list
         return ''.join(temp) #Changes list from list to string
 
     def genuser():
         word_list = gen.word_list() #gets the word list
-        return "".join(word_list[random.randrange(0, len(word_list))]) + "".join(word_list[random.randrange(0, len(word_list))]) + str(random.randrange(100, 999)) #returns two words and a random 3 digit number together
+        return "".join(random.choice(word_list)).join(random.choice(word_list)) + str(random.randrange(100, 999)) #returns two words and a random 3 digit number together
     
     def genuser_password(amount, paslength):
         for i in range(0, amount): #loops for as many times in amount
             user = gen.genuser() #gets a random username
             password = gen.genpassword(paslength) #gets a random password with determined length
             print(f"Your username is: {user} | Your password is: {password}") #outputs it into console
-
 
 gen()
